3dcbef5df7bd9f57c718ae08a8dfcdbb3227a4ff
Amir Gevari <lonag29@lonag29-m1.paconsulting.com>
2019-02-05 17:21:14 +0000

    create a costume document to send all the styles to the client

---
+++ sick-fits/frontend/pages/_document.js
@@ -0,0 +1,28 @@
+// this file is a costume document which is collecting all the style form the styled component and send it to the client
+// and it is fixing te showing the unsltyled page for a split second.
+
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
