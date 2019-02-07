3ae1464355f500b70263f139d912d6f8a462e3d8
Amir Gevari <lonag29@lonag29-m1.paconsulting.com>
2019-02-07 10:55:49 +0000

    added relation between db and the schema and server

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+//this file connecting to the prisma and let us use all of the different mutations and queries in JS
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
