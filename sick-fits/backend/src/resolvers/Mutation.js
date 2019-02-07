8ad8f429b0b69e1d467cc783f80b28cff0843e78
Amir Gevari <lonag29@lonag29-m1.paconsulting.com>
2019-02-07 10:59:39 +0000

    write a couple of mutation and qury to play with graphql

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,41 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const update = { ...args };
+    delete update.id;
+    const item = await ctx.db.mutation.updateItem(
+      {
+        data: update,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+    return item;
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const update = { ...args };
+    delete update.id;
+    const item = await ctx.db.mutation.deleteItem(
+      {
+        data: update,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
