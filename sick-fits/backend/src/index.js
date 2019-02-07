ec01c77c02c75506866a9ffd6bf63f6fab69dc10
Amir Gevari <lonag29@lonag29-m1.paconsulting.com>
2019-02-07 10:58:12 +0000

    create the server in node

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,20 @@
 // let's go!
+
+require("dotenv").config({ path: "./variable.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+const port = process.env.PORT;
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  () => {
+    console.log(`server is running on http://localhost:${port}`);
+  }
+);
